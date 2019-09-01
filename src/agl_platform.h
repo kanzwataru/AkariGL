545dc5ba6dc071b7edfef67976b78d289ebe6b69
神崎航 <kanzwataru@gmail.com>
2019-09-01 11:43:21 -0400

    * obj loading

--- src/agl_platform.h
+++ src/agl_platform.h
@@ -3,7 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 char *agl_filesys_load(const char *path, size_t *size_out);
+float *agl_filesys_obj_load(const char *path, size_t *vert_num);
 
 #endif /* AKARI_PLATFORM_H */
--- src/testing.c
+++ src/testing.c
@@ -1,10 +1,13 @@
 #include "agl.h"
 #define DEBUG
 #include "renderer/debug.h"
+#define TINYOBJ_LOADER_C_IMPLEMENTATION
+#include "tinyobj_loader_c.h"
 
 #define WIDTH  800
 #define HEIGHT 600
 
+/*
 static float quad_verts[] = {
     0.5f,   0.5f, 0.0f,
     0.5f,  -0.5f, 0.0f,
@@ -67,12 +70,17 @@ static struct MeshData quad_data = {
     sizeof(quad_verts) / sizeof(float) / 3,
     sizeof(quad_elements) / sizeof(GLuint)
 };
+*/
 
-static struct {
+struct Model {
+    float *data;
+    size_t vert_count;
     GLuint vao;
     GLuint vbo;
     mat4 model;
-} cube_info;
+};
+
+static struct Model cube_info;
 
 static ShaderID debug_shader;
 static ShaderID flat_shader;
@@ -81,21 +89,79 @@ static MeshID   quad_id;
 static vec3 light_dir = {0,0,0};
 static unsigned int counter;
 
-void upload_cube(void)
+void load_obj(const char *path, float **data, size_t *out_vert_count)
 {
-    glGenVertexArrays(1, &cube_info.vao);
-    glGenBuffers(1, &cube_info.vbo);
+    char *obj_file;
+    size_t obj_file_size;
 
-    glBindVertexArray(cube_info.vao);
+    tinyobj_material_t *materials;
+    tinyobj_shape_t *shapes;
+    tinyobj_attrib_t attrib;
+    size_t material_count;
+    size_t shape_count;
 
-    glBindBuffer(GL_ARRAY_BUFFER, cube_info.vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
+    obj_file = agl_filesys_load(path, &obj_file_size);
+    assert(obj_file);
+    obj_file_size = strlen(obj_file);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+    int ret = tinyobj_parse_obj(&attrib, &shapes, &shape_count, &materials, &material_count, obj_file, obj_file_size, TINYOBJ_FLAG_TRIANGULATE);
+    if(ret != 0) {
+        fprintf(stderr, "obj import failed: %d\n", ret);
+        exit(EXIT_FAILURE);
+    }
+
+    *data = calloc(attrib.num_faces, 8 * sizeof(float));
+    assert(data);
+
+    *out_vert_count = attrib.num_faces;
+
+    size_t face_offset = 0;
+    float *tri = *data;
+    for(size_t i = 0; i < attrib.num_faces; ++i) {
+        tinyobj_vertex_index_t id = attrib.faces[i];
+        tri[0] = attrib.vertices[3 * (size_t)id.v_idx + 0];
+        tri[1] = attrib.vertices[3 * (size_t)id.v_idx + 1];
+        tri[2] = attrib.vertices[3 * (size_t)id.v_idx + 2];
+
+        tri[3] = attrib.normals[3 * (size_t)id.vn_idx + 0];
+        tri[4] = attrib.normals[3 * (size_t)id.vn_idx + 1];
+        tri[5] = attrib.normals[3 * (size_t)id.vn_idx + 2];
+
+        tri[6] = attrib.texcoords[2 * (size_t)id.vt_idx + 0];
+        tri[7] = attrib.texcoords[2 * (size_t)id.vt_idx + 1];
+
+        tri += 8;
+        //face_offset += (size_t)attrib.face_num_verts[i];
+    }
+}
+
+void upload_model(struct Model *model)
+{
+    glGenVertexArrays(1, &model->vao);
+    glGenBuffers(1, &model->vbo);
+
+    glBindVertexArray(model->vao);
+    glBindBuffer(GL_ARRAY_BUFFER, model->vbo);
+    glBufferData(GL_ARRAY_BUFFER, model->vert_count * (8 * sizeof(float)), model->data, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    //glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    //glEnableVertexAttribArray(2);
+}
+
+void print_model(struct Model *model)
+{
+    for(size_t i = 0; i < model->vert_count; ++i) {
+        printf("[%5d] v %-5f %-5f %-5f \t vn %-5f %-5f %-5f \t vt %-5f %-5f\n", i,
+               model->data[0 + (i * 8)], model->data[1 + (i * 8)], model->data[2 + (i * 8)],
+               model->data[3 + (i * 8)], model->data[4 + (i * 8)], model->data[5 + (i * 8)],
+               model->data[6 + (i * 8)], model->data[7 + (i * 8)]);
+    }
 }
 
 void draw_scene(ShaderID shader)
@@ -174,12 +240,16 @@ int main(void)
     glEnable(GL_CULL_FACE);
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    quad_id = agl_upload_new_mesh(&quad_data);
+    //quad_id = agl_upload_new_mesh(&quad_data);
     debug_shader = agl_load_compile_shader("res/shaders/debug.vert", NULL, "res/shaders/debug.frag");
     flat_shader = agl_load_compile_shader("res/shaders/flat.vert", NULL, "res/shaders/flat.frag");
     shad_shader = agl_load_compile_shader("res/shaders/shadow.vert", "res/shaders/shadow.glsl", "res/shaders/shadow.frag");
 
-    upload_cube();
+    load_obj("res/meshes/cube.obj", &cube_info.data, &cube_info.vert_count);
+    //cube_info.data = agl_filesys_obj_load("res/meshes/cube.obj", &cube_info.vert_count);
+    assert(cube_info.data);
+    upload_model(&cube_info);
+    print_model(&cube_info);
     glm_mat4_identity(cube_info.model);
 
     glClearColor(0.2f, 0.3f, 0.3f, 0.0f);
@@ -199,6 +269,11 @@ int main(void)
         glDepthMask(GL_TRUE);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
+        //draw_fullscreen();
+        draw_scene(flat_shader);
+
+
+        /*
         //glCullFace(GL_BACK);
         //draw_scene(shad_shader);
 
@@ -231,6 +306,7 @@ int main(void)
 
         // shaded render
         glDisable(GL_STENCIL_TEST);
+        */
 
         agl_window_swap_buffers();
     }
@@ -238,5 +314,7 @@ int main(void)
     agl_renderer_quit();
     agl_window_quit();
     
+    free(cube_info.data);
+
     return 0;
 }
