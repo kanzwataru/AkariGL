91f49431a261cfd6e2869c0fa6e31634ff2da9f7
神崎航 <kanzwataru@gmail.com>
2019-09-03 09:26:12 -0400

    * depth buffer usable in post-process

--- res/shaders/post_direct.frag
+++ res/shaders/post_direct.frag
@@ -3,6 +3,8 @@ in vec2 tex_coord;
 out vec4 out_color;
 
 uniform sampler2D col_buffer;
+uniform sampler2D depth_buffer;
+uniform sampler2D stencil_buffer;
 
 void main()
 {
--- src/testing.c
+++ src/testing.c
@@ -207,11 +207,21 @@ void draw_fullscreen(void)
     glDrawArrays(GL_TRIANGLES, 0, 6);
 }
 
-void draw_postprocess(ShaderID shader, GLuint col_buffer)
+void draw_postprocess(ShaderID shader, GLuint col_buffer, GLuint depth_buffer)
 {
     glUseProgram(shader);
     glBindVertexArray(screen_quad.vao);
+    int col_buffer_loc = glGetUniformLocation(shader, "col_buffer");
+    glUniform1i(col_buffer_loc, 0);
+    int depth_buffer_loc = glGetUniformLocation(shader, "depth_buffer");
+    glUniform1i(depth_buffer_loc, 1);
+
+    glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, col_buffer);
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, depth_buffer);
+
     glDrawArrays(GL_TRIANGLES, 0, 6);
 }
 
@@ -246,7 +256,7 @@ int main(void)
     glm_scale(suzanne_info.model, (vec4){0.5f, 0.5f, 0.5f, 1.0f});
 
     // set up framebuffer
-    GLuint framebuffer, col_buffer, depthstencil_rbuffer;
+    GLuint framebuffer, col_buffer, depth_buffer, depthstencil_rbuffer;
     glGenFramebuffers(1, &framebuffer);
     glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
 
@@ -258,11 +268,21 @@ int main(void)
     glBindTexture(GL_TEXTURE_2D, 0);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, col_buffer, 0);
 
+    glGenTextures(1, &depth_buffer);
+    glBindTexture(GL_TEXTURE_2D, depth_buffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, WIDTH, HEIGHT, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, depth_buffer, 0);
+
+    /*
     glGenRenderbuffers(1, &depthstencil_rbuffer);
     glBindRenderbuffer(GL_RENDERBUFFER, depthstencil_rbuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, WIDTH, HEIGHT);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_STENCIL, WIDTH, HEIGHT);
     glBindRenderbuffer(GL_RENDERBUFFER, 0);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, depthstencil_rbuffer);
+    */
 
     glEnable(GL_DEPTH_TEST);
     //glEnable(GL_CULL_FACE);
@@ -349,7 +369,7 @@ int main(void)
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
         glClear(GL_COLOR_BUFFER_BIT);
         glDisable(GL_DEPTH_TEST);
-        draw_postprocess(post_direct_shader, col_buffer);
+        draw_postprocess(post_direct_shader, col_buffer, depth_buffer);
 #endif
         agl_window_swap_buffers();
     }
